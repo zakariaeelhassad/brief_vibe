1e377583e86bf3acfcdfc1b37aac1f03d2a2a7fc
zakariaeelhassad <zakariaelhassad031@gmail.com>
2025-02-26 16:46:04 +0000

    ajoute model pour follow

---
+++ app/Models/Follow.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Follow extends Model
+{
+    use HasFactory;
+
+    protected $table = 'follows'; 
+
+    protected $fillable = ['follower_id', 'following_id', 'status'];
+
+    public function follower()
+    {
+        return $this->belongsTo(User::class, 'follower_id');
+    }
+
+    public function following()
+    {
+        return $this->belongsTo(User::class, 'following_id');
+    }
+}
