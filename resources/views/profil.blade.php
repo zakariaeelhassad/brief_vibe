7418c00c3a62a89cdf9eb5bdff789112b3534b2b
zakariaeelhassad <zakariaelhassad031@gmail.com>
2025-02-26 16:50:35 +0000

    ajoute une page blade pour l'affichage de profil

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+
+<div class="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden border border-gray-200 p-6 text-center">
+    <div class="bg-white w-full max-w-lg rounded-lg shadow-2xl overflow-hidden p-8 text-center transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-xl">
+        <img src="{{ asset('storage/' . $user->profile_picture) }}" alt="Profile Picture"  class="w-32 h-32 mx-auto rounded-full border-4 border-indigo-500 mb-6">
+        
+        <h2 class="text-3xl font-semibold text-gray-800 mb-2">{{ $user->name }}</h2>
+        <p class="text-gray-500 text-sm mb-4"></p>
+    
+        <div class="text-left text-gray-600">
+            <p class="text-sm"><strong>Email:</strong> {{ $user->email }}</p>
+        </div>
+    
+        <form action="{{ route('follow' , $user->id) }}" method="POST">
+            @csrf
+            <button type="submit" class="mt-6 inline-block bg-indigo-600 text-white py-2 px-6 rounded-lg text-lg hover:bg-indigo-700 transition duration-300">Follow</button>
+        </form> 
+    </div>
+</div>
+
+
+@endsection
